243db7fe6ec5a52729ac2f12bec46c846199dfc8
Isaaccna <isaac.andrade1231@gmail.com>
2023-02-05 21:38:23 +0000

    done

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ReturnArrayContents {
     /**
      * Return a string with an array's contents, formatted with spaces.
@@ -28,6 +30,17 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+           
+        StringBuilder str = new StringBuilder();
+        String separator = " ";
+
+        for (int i = 0; i < nums.length; i ++) {
+            
+            str.append(separator).append(nums[i]);
+            
+        }
+        
+        
+        return str.toString() ;
     }
 }
